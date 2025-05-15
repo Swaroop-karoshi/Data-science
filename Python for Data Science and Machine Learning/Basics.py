c43e308b117b219b3cae627724c2b2ceb354204d
Swaroop-karoshi <swaroopkaroshi@gmail.com>
2025-05-15 23:00:31 +0530

    basics

---
+++ Python for Data Science and Machine Learning/Basics.py
@@ -0,0 +1,15 @@
+#x() is the function, weather input or output function,
+# Python is the case-sensitive language
+print("Hello world")
+
+#print(hello world) is wrong because it is string
+
+print(7)
+print(1.7)
+print(True)                               # "T" should be Capital in True
+print("hello", 7, 1.7, True)              # Here space is shown between the output
+print('hello', 2, 7.7, True, sep='/')     # Here there is / between
+print('Hello', end='-')                   # By-Default there is /n in end so the next print is shown in next line
+print('world')                            # By adding - in end the output is shown in same line the - at the end of hello
+
+# DATA TYPES
