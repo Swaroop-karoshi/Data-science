2a02a459714bf0517905758a4d26372e1b5e21ed
Swaroop-karoshi <swaroopkaroshi@gmail.com>
2025-05-16 18:34:04 +0530

    Python Basics Learning

--- Python for Data Science and Machine Learning/Basics.py
+++ Python for Data Science and Machine Learning/Basics.py
@@ -1,6 +1,6 @@
-#x() is the function, weather input or output function,
+# x() is the function, weather input or output function,
 # Python is the case-sensitive language
-from numpy.testing.print_coercion_tables import print_new_cast_table
+from cgi import print_environ_usage
 
 print("Hello world")
 
@@ -46,13 +46,13 @@ print((1,2,3,4,5))
 print({1,2,3,4,5})
 
 # Dictionary
-print({'name': 'Swarooop', 'gender':'Male', 'Weight':62})
+print({'name': 'Swaroop', 'gender':'Male', 'Weight':62})
 
 # type
 print(type(3))
 print(type('String'))
 print(type(2.5))
-print(type(3+3j))       # i is not allowed in complex number
+print(type(3+3j))       # 'i' is not allowed in complex number
 print(type([1,2,3]))
 
 # Variables    (Containers for future use)
@@ -65,7 +65,7 @@ b=7
 
 print(a + b)
 
-# Dynamic Typing i.e python understands data types for the variable
+# Dynamic Typing i.e. python understands data types for the variable
 a=5
 # Static Typing
 # int a = 5 which is not used in python
@@ -89,3 +89,73 @@ print(a,b,c)
 
 a=b=c=5
 print(a, b, c)
+
+# Keywords & Identifiers
+# Keywords
+
+# False      await      else       import     pass
+# None       break      except     in         raise
+# True       class      finally    is         return
+# and        continue   for        lambda     try
+# as         def        from       nonlocal   while
+# assert     del        global     not        with
+# async      elif       if         or         yield
+
+# Given above are the 35 keywords of Python which shouldn't be used as a Variable
+
+# Identifiers
+#You can't start with a digit
+name1='swaroop'
+print(name1)
+
+# You can use special char -> _ (Underscore)
+_='swaroop'
+print(_)
+# identifiers can't be any keywords
+
+
+# User Input
+
+# Static v/s Dynamic Software
+# Static doesn't interact
+
+input('Enter your name:')
+input('Enter email:')
+
+# take input from user and store them in a variable
+fnum = input('Enter the first number ')
+snum = input('Enter the second number ')
+print(type(fnum), type(snum))
+# add the 2 variables
+result = fnum+snum
+# print the result
+print(result)
+# Here the output will be in string that is if we add 45 and 45 here output will be 4545 because input default is string
+
+# Implicit Vs Explicit
+# Implicit
+print(5+5.6)
+print(type(5), type(5.6))
+
+# Explicit
+print(int('4'))        # str --> int
+print(type(int('4')))
+print(int(4.5))        # Here output will be 4
+# int(4+5j)     # is error because it is not possible to cinvert to int
+
+# int to string
+
+print(str(5))
+print(type(str(5)))
+
+#Float
+
+print(float(4))     # Here output is 4.0
+
+# Take input from user and store them in a variable
+fnum = input("Enter first number: ")
+snum = input("Enter Second number: ")
+# add two variables
+result = int(fnum) + int(snum)
+print(result)
+
