eb7ec180e9866e9d6d3e7a1eced1458465a080b5
Swaroop-karoshi <swaroopkaroshi@gmail.com>
2025-07-25 10:11:55 +0530

    String Exercise

--- Python for Data Science and Machine Learning/008_ConditionalExpression.py
+++ Python for Data Science and Machine Learning/008_ConditionalExpression.py
@@ -1,4 +1,5 @@
 # Conditional Expression
+from unicodedata import category
 
 # Single line condition of positive or negative
 num = 5
@@ -12,3 +13,22 @@ num = 6
 result = "Even" if num % 2 == 0 else "Odd"
 
 print(result)
+
+a = 6
+b = 7
+
+max_num = a if a>b else b
+print(max_num)
+
+min_num = a if a<b else b
+print(min_num)
+
+age = 13
+
+category = "adult" if age >= 18 else "Child"
+print(category)
+
+user_role = "Admin"
+
+access = "Full Access" if user_role == "Admin" else "Limited Access"
+print(access)
